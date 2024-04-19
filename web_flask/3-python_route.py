75088d6eebfd3ca054613de50f3207dbf4769c2d
khloud54 <kh.t705042000@gmail.com>
2024-04-19 13:24:41 -0700

    lo

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,16 +6,17 @@ Starts a Flask-based web application.
 
 from flask import Flask
 from werkzeug.utils import escape
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-
     """
 
     Displays 'Hello HBNB!' when acessing root URL.
     """
+
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
@@ -43,6 +44,8 @@ def display_python(text):
     Default value of text is 'is cool'.
     """
     return 'python {}'.format(escape(text.replace('_', ' ')))
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)

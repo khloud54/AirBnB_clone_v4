35700256ff719c2cb17b8c41b34a17823eae3278
khloud54 <kh.t705042000@gmail.com>
2024-04-20 09:50:15 -0700

    fb

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """
-Initiates a Flask-based web application.
+Script that starts a Flask web application.
 """
 
-
-
-from fask import Flask, render_template
-from models import *
+from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """Exhibit the states and cities in alphabetical order."""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
 
-@app.teardown_apppcontext
-def teardown_db(exception):
-    """Shuts down the storage on teardown"""
+@app.route('/states', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('9-states.html', states=sorted_states)
+
+
+@app.route('/states/<state_id>', strict_slashes=False)
+def state_cities(state_id):
+    state = storage.get(State, state_id)
+    if state:
+        return render_template('9-states.html', state=state)
+    else:
+        return render_template('9-states.html', not_found=True)
+
+
+@app.teardown_appcontext
+def teardown(exception):
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

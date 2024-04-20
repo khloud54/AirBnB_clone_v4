f07f61040c7be533570b1e494213df49d3409221
khloud54 <kh.t705042000@gmail.com>
2024-04-20 04:35:52 -0700

    kh

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@ Initiates a Flask-based web application.
 """
 
 from flask import Flask, render_template
-from models import storage
+from ..models import storage
 from models.state import State
 
 app = Flask(__name__)

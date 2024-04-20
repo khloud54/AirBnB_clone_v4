b35e4bd8aaa55138dc251e0df2c050bab941939a
khloud54 <kh.t705042000@gmail.com>
2024-04-20 09:36:36 -0700

    dv

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,7 @@ def states_list():
     """ display a HTML page with the states listed in alphabetical order"""
     states = sorted(list(storage.all("State").values()), key=lambda x:
 x.name)
-    return render_template(7'-states_list.html', states=states)
+    return render_template('7-states_list.html', states=states)
 
 @app.teardown appcontext
 def teardown db(exception):

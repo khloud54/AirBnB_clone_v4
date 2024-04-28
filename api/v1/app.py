3af2c2fbc020f7fd41127bb7a1e2f3c3cafc6111
khloud54 <kh.t705042000@gmail.com>
2024-04-28 02:46:29 -0700

    l

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
 """
-The RESTful api starts here. The api aids data access in the app.
+create Flask app; and register the blueprint app_views to Flask instance app.
 """
 from os import getenv
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-
-from api.v1.views import app_views
+from flask import Flask
 from models import storage
+from api.v1.views import app_views
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+@app = Flask(__name__)
+
 app.register_blueprint(app_views)
-CORS(app, origins=["0.0.0.0"])
-host = getenv("HBNB_API_HOST", "0.0.0.0")
-port = getenv("HBNB_API_PORT", "5000")
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """Cleanup operations"""
-    storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({"error": "Not found"}), 404
-
-
-if __name__ == "__main__":
-    app.run(host, port, threaded=True, debug=True)
+if__name__=='__main__':
+  HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+  PORT = int(getenv('HBNB_API_PORT', 5000))
+  app.run(hosts=HOST, port=Port, threaded=True)
